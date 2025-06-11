20b1294c3208d6e84c1b5b0174c8e1eb4ad1c781
AbdelrahmanAli <abdelrahmanali2310@gmail.com>
2025-06-11 11:42:26 +0300

    create post model and create post table by migration create postFactory and add it in databaseSeeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->paragraph,
+            'user_id' => User::findOrFail(
+                $this->faker->numberBetween(1, User::count())
+            )->id,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
--- database/migrations/2025_06_11_081131_create_posts_table.php
+++ database/migrations/2025_06_11_081131_create_posts_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
